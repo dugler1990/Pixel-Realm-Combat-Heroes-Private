ea3fba35c22ec3cd1e8391dcd86d808a04a8f4f1
fresh <douglas.j.martin1990@gmail.com>
2026-05-23 16:13:20 +0200

    pre refactor of tmx init to unify it ( some danger)

---
+++ levels/tmx/can_build_tiles.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="can_build_tiles" tilewidth="550" tileheight="550" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <properties>
+  <property name="deep_snow" value="true"/>
+ </properties>
+ <tile id="1">
+  <image width="550" height="550" source="../../Graphics/rts_placeholders/deep_snow.png"/>
+ </tile>
+</tileset>
